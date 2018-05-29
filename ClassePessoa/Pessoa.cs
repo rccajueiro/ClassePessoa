5bef9a579c01adeea33d622a167c71f771f4996a
Ricardo Cajueiro <ricardo@rcajueiro.eti.br>
2018-05-29 17:36:06 -0300

    Commit Initial

---
+++ ClassePessoa/Pessoa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassePessoa
+{
+    public abstract class Pessoa
+    {
+        private int codigo;
+        private string nome;
+
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ ClassePessoa/PessoaFisica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassePessoa
+{
+    class PessoaFisica : Pessoa
+    {
+        private string cpf;
+
+        public string Cpf { get; set; }
+
+        public PessoaFisica(int Codigo, string Nome, string Cpf)
+        {
+            this.Codigo = Codigo;
+            this.Nome = Nome;
+            this.Cpf = Cpf;
+        }
+    }
+}
